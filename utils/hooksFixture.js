6ef763cc775f1424d15f006b6caf36b2ec4e0dcf
Girishgktester <girishgk.tester@gmail.com>
2025-04-26 07:58:47 +0530

    changes

--- utils/hooksFixture.js
+++ utils/hooksFixture.js
@@ -1,17 +1,14 @@
-const { test } = require('@playwright/test');  // Add this line to import Playwright's test object
-const fs = require('fs');
-const path = require('path');
+const { test } = require('@playwright/test');
 const { urls } = require('../utils/constants');
 const getPages = require('../utils/pageFactory');
-const config = require('../configs/configLoader'); //  loaded config
-
+const config = require('../configs/configLoader');
 
 const logger = {
   info: (msg) => console.log(`ℹ[INFO] ${new Date().toISOString()} - ${msg}`),
   error: (msg) => console.error(`[ERROR] ${new Date().toISOString()} - ${msg}`),
 };
 
-exports.test = test.extend({  // Change base to test here
+exports.test = test.extend({
   isolatedContext: async ({ browser }, use) => {
     const context = await browser.newContext();
     await use(context);
@@ -21,12 +18,9 @@ exports.test = test.extend({  // Change base to test here
   hooks: async ({ isolatedContext }, use, testInfo) => {
     const context = isolatedContext;
     const page = await context.newPage();
-    
-    // const envSpecificURL = process.env.ENV === 'qa' ? urls.qa : urls.dev; // Example for dynamic URL based on ENV
-    // await page.goto(envSpecificURL, { timeout: 30000 });  // Adding a timeout to avoid hanging
 
-    await page.goto(config.baseURL);  // Assuming you have defined this in constants.js
-    const pages = getPages(page);  // Factory method for additional helpers
+    await page.goto(config.baseURL);
+    const pages = getPages(page);
 
     try {
       await use({ page, ...pages });
@@ -35,17 +29,13 @@ exports.test = test.extend({  // Change base to test here
       logger.error(err.stack || err.message);
       throw err;
     } finally {
-      if (testInfo.status === 'failed') {
-        const screenshotDir = path.join(__dirname, '..', 'screenshots');
-        if (!fs.existsSync(screenshotDir)) fs.mkdirSync(screenshotDir, { recursive: true });
-
-        const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
-        const filePath = path.join(screenshotDir, `${testInfo.title.replace(/\s+/g, '_')}_${timestamp}.png`);
-
-        await page.screenshot({ path: filePath });
-        logger.info(`Saved failure screenshot: ${filePath}`);
-      }
-
+      //Take screenshot and attach to Playwright report (for all tests)
+      const screenshotBuffer = await page.screenshot();
+      await testInfo.attach('screenshot', {
+        body: screenshotBuffer,
+        contentType: 'image/png',
+      });
+      logger.info('Attached screenshot to the Playwright report');
       logger.info('Clearing cookies and storage...');
       await page.context().clearCookies();
       await page.evaluate(() => {
